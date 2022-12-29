0590b61e8481c9dc1680d2bfbfab4433e5f23401
harinisf <121146560+harinisf@users.noreply.github.com>
2022-12-29 10:50:47 +0530

    MetroCardManagement

---
+++ MetroCardManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MetroCardManagement;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        FilesCSV.Create();
+        //Operation.DefaultDate();
+        FilesCSV.ReadFromCSV();
+        Operation.MainMenu();
+        FilesCSV.WriteToFiles();
+    }
+}
