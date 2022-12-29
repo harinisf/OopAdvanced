e452950fb9db04ec7f2eafb132a3d2b4695cc643
harinisf <121146560+harinisf@users.noreply.github.com>
2022-12-29 10:47:00 +0530

    sorting ascending and descending

---
+++ Sorting/AscendingSort/BubbleSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace BubbleSort;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] arr = new int[]{45,33,12,55,77,22,33,14,67,12,35};
+        int temp;
+        for(int i=0;i<arr.Length-1;i++)
+        {
+            for(int j=0;j<arr.Length-1;j++)
+            {
+                if(arr[j] > arr[j+1])
+                {
+                    temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.Console.Write("Ascending Order in Bubble Sort ");
+        foreach(int i in arr)
+        {
+            System.Console.WriteLine(i);
+        }
+    }
+}
